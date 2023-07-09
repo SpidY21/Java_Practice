f225febeb8fa12d7c535c3cdbedc6e56814053df
SpidY21 <yashkumarroy164@gmail.com>
2023-07-09 11:40:52 +0530

    practice java codes

---
+++ src/P7.java
@@ -0,0 +1,26 @@
+public class P7 {
+    public static int ans(int k,int n,int[] arr){
+        int count=0;
+        int j=1;
+        int sum=0;
+        while(count<=k){
+            for(int i=0;i<n;i++){
+                if(arr[i]%j==0){
+
+                    sum+=j;
+                }
+                count++;
+                j++;
+//                else{
+//                    break;
+//                }
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[]arr={2,3,4,6,5};
+        System.out.println(ans(4,5,arr));
+    }
+}
