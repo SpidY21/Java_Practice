32fe5f68b4dc51bf4055f84d00930f8cdeb3fa9d
SpidY21 <yashkumarroy164@gmail.com>
2023-07-15 17:12:38 +0530

    practice java codes

--- src/TCS3.java
+++ src/TCS3.java
@@ -1,10 +1,20 @@
 public class TCS3 {
-    int maximumSum(int[] arr){
-        int sum=0;
-        int maxsum=0;
+    public static int maximumSum(int[] arr){
+        int sum1=0;
+        int sum2=0;
         for(int i=0;i<arr.length;i++){
-
+            sum1+=arr[i];
+            i++;
         }
-        return 1;
+        for(int i=1;i<arr.length;i++){
+            sum2+=arr[i];
+            i++;
+        }
+        return Math.max(sum1,sum2);
+    }
+
+    public static void main(String[] args) {
+        int arr[]= {1,2,3,4,5,6,7,8,9,10};
+        System.out.println(maximumSum(arr));
     }
 }
